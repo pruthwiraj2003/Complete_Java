836e06da5cd686d900a44c5c55e749ca0d5cdace
Pruthwiraj mahapatra <pruthwirajmahapatra2@gmail.com>
2026-08-17 21:05:33 +0530

    Adding some new file

--- Linked_List/LL.java
+++ Linked_List/LL.java
@@ -1,68 +1,23 @@
 public class LL{
-
-  private Node head ;
-  private Node tail;
-
-  private int size;
-  public LL(){
-    this.size = 0;
-  }
-  public void insertLast(int val){
-    if(tail==null){
-      insertFirst(val);
-      return ;
-    }
-    Node node = new Node(val);
-    tail.next = node;
-    tail = node ;
-    size++;
-  }
-  public void insertFirst(int val){
-    Node node = new Node(val);
-    node .next = head;
-    head = node ;
-    if(tail==null){
-      tail = head;
-    }
-
-    size+=1;
-    
-  }
-
-  public void insert(int val , int index){
-    if(index ==0){
-      insertFirst(val);
-      return ;
-
-    }
-    if(index == size){
-      insertLast(val);
-      return;
-    }
-    Node temp = head;
-  }
-
-  public void Display(){
-    Node temp = head;
-    while(temp!= null){
-      System.out.print(temp.value+" ->");
-      temp= temp.next;
-    }
-    System.out.print("END");
-    // System.out.println(size);
-  }
-
-  private class Node{
-    private int value ;
+    private Node head;
+    Private Node tail;
+    private class Node{
+    private int value;
     private Node next;
 
     public Node(int value){
-      this.value = value;
-    }
-
-    public Node(int value, Node next){
-      this.value = value;
-      this.next = next ;
-    }
+    this.value = value;
   }
+
+  public Node(int value , Node next){
+    this.value = value;
+    this.next = next;
+
+  }
+
+  }
+
+  
+
+  
 }
